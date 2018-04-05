940ebd8ec74c8063b230374c7528a5bcb8f615f6
Alex Zimin <alex.zimin@cognitivenetworks.com>
2018-04-05 08:41:38 -0700

    add-tags

--- add-tags.py
+++ add-tags.py
@@ -15,7 +15,10 @@ class ArgsParser(argparse.ArgumentParser):
             'description',
             'Adds tags to ec2 instances by name or id')
         argparse.ArgumentParser.__init__(self, *args, **kwargs)
+        self.formatter_class = argparse.RawTextHelpFormatter
         self.options = None
+        self.epilog = '''For example\n add-tags.py name1 name2 name2 --tags tag1=a tag2=b tag3=c
+                    '''
         self.add_argument('-r', '--regions', dest='region', default='us-west-2', help='Region to connect')
         self.add_argument('-p', '--profile', dest='profile', help='Profile to use')
         self.add_argument('--dry-run', dest='dry_run', action='store_true', default=False,
